f5cea83e48075e23b5ab67ab6d4c412af6930531
Edo -maland- <Screetsec@users.noreply.github.com>
2016-06-27 11:09:24 +0700

    Update Lalin.sh

--- Lalin.sh
+++ Lalin.sh
@@ -3133,7 +3133,7 @@ echo -e $white"             % $cyan           LAZY "$red"LINUX$cyan TOOLKIT $red
 echo -e $white"             % $cyan  Tested On $red Kali sana v.2 $cyan and "$red"rolling$white   %"
 echo -e $white"             % $cyan  Contact me in screetsec[at]@gmail.com$white   %"
 echo -e $white"             % $cyan    Powered by "$red"Miffly"$cyan" [ Edo -maland-] $white    %"
-echo -e $white"             % $cyan  	 Changelog: $red 26-06-2016   	$white %"
+echo -e $white"             % $cyan  	 Changelog: $red 27-06-2016   	$white %"
 echo -e $white"             %                                           %"
 echo -e $white"             ============================================="
 echo
